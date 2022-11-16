fa10e2b0a9dabce3aef868257565dbc611bab5c0
Игорь <superigorek74@gmail.com>
2022-11-16 22:44:40 +0500

    + with last commit

---
+++ PlumbingStore/PlumbingStore/apps/marketapp/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from django.utils import timezone
+
+
+class Advertisment(models.Model):
+
+    class Category(models.TextChoices):
+        GOODS = 'GOODS', 'Товары'
+        SERVICES = 'SERVICES', 'Услуги'
+        HIRING = 'HIRING', 'Найм'
+
+    category = models.CharField(max_length=10,
+                                choices=Category.choices,
+                                default=Category.GOODS)
+    header = models.CharField(max_length=100)
+    description = models.TextField(max_length=5000)
+    image = models.ImageField(blank=True)
+    date_posted = models.DateTimeField(default=timezone.now)
+    user = models.ForeignKey('usersapp.User', on_delete=models.CASCADE, null=False)
+
+
+class Feedback(models.Model):
+
+    user = models.ForeignKey('usersapp.User', on_delete=models.CASCADE, null=False)
+    advert = models.ForeignKey(Advertisment, on_delete=models.CASCADE, null=False)
+    text = models.TextField(max_length=5000)
+    image = models.ImageField(blank=True)
+    date_posted = models.DateTimeField(default=timezone.now)
