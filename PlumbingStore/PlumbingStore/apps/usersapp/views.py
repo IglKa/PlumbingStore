a6000a1b739844c1abbb6643b60d3a708a705273
Игорь <superigorek74@gmail.com>
2022-11-22 22:47:16 +0500

    registration bug fixed

--- PlumbingStore/PlumbingStore/apps/usersapp/views.py
+++ PlumbingStore/PlumbingStore/apps/usersapp/views.py
@@ -1,10 +1,12 @@
 from django.contrib.auth import login, authenticate
 from django.views.generic import TemplateView, DetailView
-from django.views.generic.edit import FormView
+from django.views.generic.edit import CreateView
+from django.shortcuts import redirect
 
 from .forms import UserForm
 from .models import User
-#TODO: Исправить баги с входом и регестрацией
+#TODO: Исправить баг со входом
+
 
 # User profile page
 class ProfileView(DetailView):
@@ -17,8 +19,11 @@ class ProfileView(DetailView):
 class TemplateView(TemplateView):
     template_name = 'registration/page.html'
 
-#Todo: Подумать над использованием CreateView вместо FormView.
-class UserCreation(FormView):
+
+#Только сейчас понял отличия CreateView от FormView.
+#FormView - отображает форму, проверяет её валидность и редиректит на success_url при успехе.
+#CreateView - отображает форму, проверяет валидность, ДОБАВЛЯЕТ ФОРМУ В БД и редиректит при успехе.
+class UserCreation(CreateView):
     template_name = 'registration/registration.html'
     form_class = UserForm
     success_url = '/'
@@ -27,6 +32,6 @@ class UserCreation(FormView):
         form.save()
         email = self.request.POST['email']
         password = self.request.POST['password']
-        user = authenticate(self.request, email=email, password=password)
+        user = authenticate(self.request, username=email, password=password)
         login(self.request, user=user)
-        return super().form_valid(form)
+        return redirect(self.success_url)
