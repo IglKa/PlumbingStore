779b005434676b147108b21dd815014d83192610
Игорь <superigorek74@gmail.com>
2022-12-03 22:05:35 +0500

    Company models added

--- PlumbingStore/shopapp/models.py
+++ PlumbingStore/shopapp/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class Company(models.Model):
+    class Category(models.TextChoices):
+        SHOP = 'SHOP', 'Shop'
+        STARTUP = 'STARTUP', 'Startup'
+        BUSINESS = 'BUSINESS', 'Business'
+
+    holder = models.ForeignKey('usersapp.User', on_delete=models.CASCADE, null=True)
+    name = models.CharField(max_length=50, blank=False)
+    descr = models.TextField(max_length=5000, blank=False)
+    header_image = models.ImageField(blank=True)
+    profile_image = models.ImageField(blank=True)
+
+    category = models.CharField(max_length=10,
+                                choices=Category.choices,
+                                default=Category.SHOP
+                                )
+
+    slug = models.SlugField(max_length=100,
+                            unique=True,
+                            db_index=True,
+                            verbose_name='URL'
+                            )
