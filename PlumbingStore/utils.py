908587347c79d1b6db04c7520028e5a1e27a8da1
Игорь <superigorek74@gmail.com>
2022-12-13 23:00:36 +0500

    Some random code

--- PlumbingStore/utils.py
+++ PlumbingStore/utils.py
@@ -18,7 +18,6 @@ class AddContextMixin:
         return context
 
 
-# TODO: rethink what to do with that and rid off of this tinki-winki ass-shit
 class SlugHandle:
     """
     This will be an automatic slug maker.
@@ -34,30 +33,35 @@ class SlugHandle:
                          '?', '%', '>', '<']
 
     def __init__(self, **kwargs):
-        self.kwargs = {}
         for key, value in kwargs:
-            self.kwargs = self.kwargs | dict(key, value)
+            if value != None or False:
+                self.kwargs = self.kwargs | dict(key, value)
+                continue
+            return self.kwargs
 
-        self.sep = getattr(self, 'sep', default='-')
-        self.low = getattr(self, 'low', default=True)
-        self.upp = getattr(self, 'upp', default=False)
-        # If user is given setting it as attr
-        if user in kwargs:
-            setattr(self, 'user', user)
-        else:
-            pass
+    def _get_user(self):
+        if user in self.kwargs:
+            self.user = str(user)
+            return self.user[:self.user.find('@')]
+        return None
 
-    def _normalize_user(self):
-        # Checking if user is str type
-        if isinstance(self.user, str) == False:
-            self.user = str(self.user)
-        self.user = self.user
-        # Because I want my site to use email instead of usernames
-        # we need to find '@' symbol and remove @email.com
-        self.user = self.user[:self.user.find('@')]
-        return self.user
+    def _get_sep(self):
+        if sep in self.kwargs:
+            return setattr(self, 'sep', sep)
+        return setattr(self, 'sep', '-')
+
+    def _get_upper_method(self):
+        if upper in self.kwargs:
+            return True
+        return False
+
+    def _get_text_for_slug(self):
+        text_list = []
+        for i in self.kwargs:
+            if isinstance(i, str):
+                text_list.append(i)
+            else:
+                pass
 
     def fill_slug(self):
-        # Need to write checks for attributes if they are given or not.
-        # But now I don't really know how to do this
-        return self._normalize_user() + self.sep + self._normalize_title()
+        pass
