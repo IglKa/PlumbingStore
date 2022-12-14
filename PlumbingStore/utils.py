a6a10b52faa990a2347b0ec5877522887d435f78
Игорь <superigorek74@gmail.com>
2022-12-14 23:50:48 +0500

    SlugHandle update

--- PlumbingStore/utils.py
+++ PlumbingStore/utils.py
@@ -20,44 +20,28 @@ class AddContextMixin:
 
 class SlugHandle:
     """ Auto Slug Maker """
-    # TODO: Refactor
 
-    # I have build something by myself. Feels cool. But I know that this is a goofy af code.
-    # I need to actually refactor it to get more beautiful code. It need's a lot of work on it, sadly or luckly.
-
+    # TODO: Refactor; think on making it as CustomField.
     forbidden_symbols = ['@', '.', '/', '$', '#', '*', '+',
                          '?', '%', '>', '<']
 
-    def __init__(self, **kwargs):
-        self.kwargs = {**kwargs}
+    def __init__(self, slug_text: list, **kwargs):
+        # Necessary attribute that will be forming text for slug
+        self.slug_text = slug_text
+        for key, value in kwargs:
+            setattr(self, str(key), value)
 
-        self.sep = '-'
-        self.user = None
+    # I would like to do something that will search given attributes for:
+    # 'lower', 'upper' or 'capitalize'
+    # to lately use it in forming slug. But I don't really know how to do this.
 
-    def _get_user(self):
-        if 'user' in self.kwargs:
-            self.user = str(user)
-            self.user = self.user[:self.user.find('@')]
-            return self.user
-        return self.user
-    # I don't know about this two methods, but I will probably delete it later.
+    def _methods_check(self):
+        pass
+
     def _get_sep(self):
-        if 'sep' in self.kwargs:
-            self.sep = sep
+        self.sep = getattr(self, 'sep', default='-')
         return self.sep
 
-    def _get_text_for_slug(self):
-        # Giving a list of values that are str type and will be a future slug.
-        self.text_list = []
-        for i in self.kwargs:
-            if isinstance(self.kwargs[i], str):
-                self.text_list.append(self.kwargs[i])
-        return self.text_list
-
-    def fill_slug(self):
-        # With given separator join self.text_list
-        text = self._get_sep().join(self._get_text_for_slug())
-        # While spaces are in text we need to delete it.
-        while ' ' in text:
-            text = text.replace(' ', '')
-        return text.lower()
+    def _slug_text(self):
+        self.slug_text = self.sep.join(self.slug_text)
+        return self.slug_text
