dc5a6cd4134a3a1bd0f02919c1dbdabf3d551ddb
Игорь <superigorek74@gmail.com>
2022-12-15 23:19:11 +0500

    tests commit

--- PlumbingStore/marketapp/tests.py
+++ PlumbingStore/marketapp/tests.py
@@ -1,10 +1,1 @@
 from django.test import TestCase
-
-from PlumbingStore.utils import SlugHandle
-
-
-slug = SlugHandle(slug_text=[
-    'FUCK !THIS@ BUG',
-    'YEEEEEEAAAAAAA'
-], sep='_')
-print(slug.form_slug_text())
