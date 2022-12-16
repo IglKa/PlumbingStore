3d62a22204b8d384129cf587233ba36d39243be1
Игорь <superigorek74@gmail.com>
2022-12-17 00:44:17 +0500

    lil correction

--- PlumbingStore/marketapp/urls.py
+++ PlumbingStore/marketapp/urls.py
@@ -7,7 +7,7 @@ app_name = 'marketapp'
 urlpatterns = [
     path('', MarketHome.as_view(), name='homepage'),
 
-    path('<slug:slug>', AdvertPage.as_view(), name='advert_page'),
+    path('<slug:slug>', AdvertPage.as_view(), name='advert-page'),
 
     path('company/<slug:slug>/', CompanyDetail.as_view(), name='company'),
     path('company/<slug:slug>/create-advert', CreateAdvert.as_view(), name='create-adv'),
