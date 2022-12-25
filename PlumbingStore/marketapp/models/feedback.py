8ae5fdeccf11f51cc57614b5732f90eca28fb00d
Игорь <superigorek74@gmail.com>
2022-12-25 17:29:22 +0500

    feedback advert separation end

--- PlumbingStore/marketapp/models/feedback.py
+++ PlumbingStore/marketapp/models/feedback.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 from django.utils import timezone
 
 from .star import Star
@@ -23,3 +24,6 @@ class Feedback(models.Model):
 
     def __str__(self):
         return f'{self.advert}: {self.star_given}'
+
+    def get_absolute_url(self):
+        return reverse('marketapp:advert-page', kwargs={'slug': self.advert})
--- PlumbingStore/marketapp/views/feedback.py
+++ PlumbingStore/marketapp/views/feedback.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, View
 from django.views.generic.detail import SingleObjectMixin
 
 from marketapp.models import Advertisment, Feedback
@@ -7,7 +7,7 @@ from marketapp.forms import CreateFeedbackForm
 from utils import AddContextMixin
 
 
-class FeedbackSectionView(AddContextMixin, SingleObjectMixin, ListView):
+class FeedbackContextView(AddContextMixin, SingleObjectMixin, ListView):
     template_name = 'marketapp/feedback-section.html'
 
     def get(self, request, *args, **kwargs):
@@ -16,9 +16,30 @@ class FeedbackSectionView(AddContextMixin, SingleObjectMixin, ListView):
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
+        context['advert_slug'] = self.object.slug
         context['menu'] = self.add_context()
         context['form'] = CreateFeedbackForm()
         return context
 
     def get_queryset(self):
         return self.object.feedback_set.all()
+
+
+class SendFeedbackView(CreateView):
+    form_class = CreateFeedbackForm
+    template_name = 'marketapp/feedback-section.html'
+
+    def form_valid(self, form, *args, **kwargs):
+        form.instance.user = self.request.user
+        form.instance.advert = Advertisment.objects.get(slug=self.kwargs.get('slug'))
+        return super().form_valid(form)
+
+
+class FeedbackSectionView(View):
+    def get(self, request, *args, **kwargs):
+        view = FeedbackContextView.as_view()
+        return view(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        view = SendFeedbackView.as_view()
+        return view(request, *args, **kwargs)
