080db73c9e2f3f083a0cc83b6a9741fbc85d8201
Игорь <superigorek74@gmail.com>
2022-12-27 21:14:09 +0500

    lil db rework

--- PlumbingStore/marketapp/models/company.py
+++ PlumbingStore/marketapp/models/company.py
@@ -2,6 +2,10 @@ from django.db import models
 from django.urls import reverse
 from django.utils import timezone
 
+from django.contrib.contenttypes.fields import GenericRelation
+
+from .feedback import Feedback
+
 
 class CompanyCategory(models.Model):
     """Company category"""
@@ -20,8 +24,14 @@ class Company(models.Model):
                                  help_text="Choose category of your Company. If it's not in the list just write it down and it will be created in our Data Base for future uses"
                                  )
 
-    name = models.CharField(max_length=50, blank=False)
-    descr = models.TextField(max_length=5000, blank=False)
+    name = models.CharField(max_length=50,
+                            blank=False
+                            )
+
+    descr = models.TextField(max_length=5000,
+                             blank=False
+                             )
+
     header_image = models.ImageField(blank=True)
     profile_image = models.ImageField(blank=True)
     date_created = models.DateTimeField(default=timezone.now)
@@ -29,8 +39,15 @@ class Company(models.Model):
     slug = models.SlugField(max_length=100,
                             unique=True,
                             db_index=True,
-                            verbose_name='URL')
+                            verbose_name='URL'
+                            )
 
+    company_rating = GenericRelation(Feedback,
+                                     null=True,
+                                     on_delete=models.SET_DEFAULT,
+                                     default=0,
+                                     limit_choices_to={'company': slug},
+                                     )
     def get_absolute_url(self):
         return reverse('marketapp:company', kwargs={'slug': self.slug})
 
@@ -42,11 +59,13 @@ class CompanyHolder(models.ForeignKey):
     # Made this table for more comfortable and useful search for holder
     holder = models.ForeignKey('usersapp.User',
                                on_delete=models.CASCADE,
-                               null=True)
+                               null=True
+                               )
 
     company = models.ForeignKey(Company,
                                 on_delete=models.CASCADE,
-                                null=True)
+                                null=True
+                                )
 
     def __str__(self):
         return f'{self.holder} - {self.company}'
