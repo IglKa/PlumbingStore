f5c631efe866ceec7ed45d494a75677cabd314ad
Игорь <superigorek74@gmail.com>
2023-01-03 00:58:31 +0500

    migration commit

--- PlumbingStore/usersapp/models.py
+++ PlumbingStore/usersapp/models.py
@@ -15,17 +15,35 @@ class EmployeePosition(models.Model):
 
 class User(AbstractUser):
     # There is no need to use username in site, so we will use email as username.
-    username = models.CharField(max_length=45, blank=True, unique=False, default=None, null=True)
-    email = models.EmailField(blank=False, unique=True, max_length=194)
-    phone_number = models.DecimalField(max_digits=11, decimal_places=0, blank=True, default=None, null=True)
+    username = models.CharField(max_length=45,
+                                blank=True,
+                                unique=False,
+                                default=None,
+                                null=True
+                                )
+    email = models.EmailField(blank=False,
+                              unique=True,
+                              max_length=194
+                              )
+    phone_number = models.DecimalField(max_digits=11,
+                                       decimal_places=0,
+                                       blank=True,
+                                       default=None,
+                                       null=True
+                                       )
 
     employee_position = models.ForeignKey(EmployeePosition,
                                           on_delete=models.SET_NULL,
                                           blank=True,
-                                          null=True)
+                                          null=True
+                                          )
 
     # Company where user works
-    company = models.ForeignKey(Company, on_delete=models.CASCADE, null=True)
+    company = models.ForeignKey(Company,
+                                on_delete=models.CASCADE,
+                                null=True,
+                                blank=True
+                                )
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = ["username"]
