543312594799d3544c27b130d55f2776d5510320
Игорь <superigorek74@gmail.com>
2023-01-04 21:40:53 +0500

    logic preparing

---
+++ PlumbingStore/marketapp/views/follow.py
@@ -0,0 +1,15 @@
+from django.views.generic.edit import CreateView
+
+from marketapp.models import Follow
+from marketapp.businesslogic import get_model_instance
+
+
+class FollowCompany(CreateView):
+    model = Follow
+    template_name = 'marketapp/company.html'
+
+    def form_valid(self, form):
+        user = self.request.user
+        form.instance.user = user
+        form.instance.company = get_model_instance(self.request.POST.get('slug'))
+        return super().form_valid(form)
