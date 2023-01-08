3ed6d1326df8453c2138156f627395d5e5ab9ac4
Игорь <superigorek74@gmail.com>
2023-01-08 18:58:34 +0500

    registration bug fixed

--- PlumbingStore/usersapp/views.py
+++ PlumbingStore/usersapp/views.py
@@ -1,6 +1,6 @@
-from django.views.generic import DetailView, CreateView
+from django.views.generic import DetailView, View
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.urls import reverse_lazy
+from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 
 from .models import User
@@ -16,15 +16,20 @@ class ProfileView(LoginRequiredMixin, DetailView):
     context_object_name = 'profile'
 
 
-class UserCreation(CreateView):
+class UserCreation(View):
     template_name = 'usersapp/registration.html'
     form_class = UserForm
-    success_url = reverse_lazy('marketapp:homepage')
 
-    def form_valid(self, form):
-        form.save()
-        email = form.cleaned_data.get('email')
-        password = form.cleaned_data.get('password1')
-        user = authenticate(self.request, username=email, password=password)
-        login(self.request, user=user)
-        return super().form_valid(form)
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name, {'form': self.form_class})
+
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            form.save()
+            email = form.cleaned_data.get('email')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(request, username=email, password=password)
+            login(request, user)
+            return redirect('marketapp:homepage')
+        return render(request, self.template_name, {'form': self.form_class})
